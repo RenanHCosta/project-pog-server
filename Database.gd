47da6a86b040f8626f03e4721c7fdad04d9ee445
Renan Costa <renan.a.costa@capgemini.com>
2023-10-09 17:00:51 -0300

    feat: SQLite database integration

---
+++ Database.gd
@@ -0,0 +1,58 @@
+extends Node
+
+var db # Database Object
+var db_name = "res://DataStore/database" # Path to Database
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	db = SQLite.new()
+	db.path = db_name
+	#HandleCreateAccount("fduiashsf", "fduiashsf", "fduiashsf@ava.com")
+	#HandleLogin("fduiashsf", "fduiashsf")
+
+func HandleLogin(username, password):
+	db.open_db()
+	var tableName = "accounts"
+	db.query("SELECT * FROM " + tableName + " WHERE username = '" + username + "';")
+
+	if (db.query_result.size() == 0):
+		print("Account does not exist")
+		return
+	
+	var account = db.query_result[0]
+	var retrieved_salt = account.salt
+	var hashed_password = GenerateHashedPassword(password, retrieved_salt)
+	var isPasswordCorrect = account.password == hashed_password
+	db.close_db()
+	return isPasswordCorrect
+
+func HandleCreateAccount(username, password, email):
+	db.open_db()
+	var tableName = "accounts"
+	
+	var salt = GenerateSalt()
+	var hashed_password = GenerateHashedPassword(password, salt)
+	
+	#db.query("INSERT INTO accounts (username, password, salt, email) VALUES ('" + username + "', '" + hashed_password + "', '" + salt + "', '" + email + "');")
+	if not db.insert_row(tableName, { "username": username, "password": hashed_password, "salt": salt, "email": email }):
+		print("SQL Error: " + db.error_message)
+		return false
+	db.close_db()
+	return true
+
+func GenerateSalt():
+	randomize()
+	var salt = str(randi()).sha256_text()
+	print("Salt: " + salt)
+	return salt
+	
+func GenerateHashedPassword(password, salt):
+	var hashed_password = password
+	var rounds = pow(2, 3)
+	#print("hashed password as input: " + hashed_password)
+	while rounds > 0:
+		hashed_password = (hashed_password + salt).sha256_text()
+		rounds -= 1
+	
+	#print("final hashed password: " + hashed_password)
+	return hashed_password
--- Server.gd
+++ Server.gd
@@ -6,9 +6,9 @@ extends Node
 
 var server = ENetMultiplayerPeer.new()
 var multiplayer_api : MultiplayerAPI
+
 const PORT = 4242
 const MAX_PLAYERS = 15
-
 const INITIAL_PLAYER_POSITION = Vector2(200, 200)
 
 func _ready():
